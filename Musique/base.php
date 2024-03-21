51876c62b2b70d0834915a77e7cfa96766f0e038
OusmaneAhmat <aousmane@deastanceservices.fr>
2024-03-21 10:11:02 +0100

    j'ai pu recupérer mes données

--- Musique/base.php
+++ Musique/base.php
@@ -5,24 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
     <title>musique</title>
-
-
-
-
-
-
     <style>
-        body{
-            background-color: #3498db;
-        }
         .navigation{
-            background-color: gainsboro;
+            background-color:orange;
             display: flex;
             flex-direction: row;
             justify-content: space-around;
             padding: 30px;
             font-weight: 800;
             font-family: 'Times New Roman', Times, serif;
+            color: white;
         }
         .navigation :hover {
             color: white;
@@ -43,11 +35,13 @@
             margin: 20px auto;
             border-collapse: collapse;
             background-color: white;
+        
         }
         th, td {
             padding: 10px;
             text-align: left;
-            border-bottom: 1px solid #ddd;
+            border: 1px solid #ddd;
+            
         }
         th {
             background-color: #f2f2f2;
@@ -55,8 +49,9 @@
         audio {
             margin-top: 10px;
         }
-    </style>
 
+    
+    </style>
 </head>
 <body>
     <header>
@@ -69,25 +64,54 @@
     <h1>Mes musiques</h1>
     <p>Venez découvrir ma musique préferées</p>
 
-     <?php
+       <?php 
         $serveur = "localhost";
         $utilisateur = "root"; 
         $mot_de_passe = ""; 
         $base_de_donnees = "base";
         $connexion = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
-        if (!$connexion) { 
-            die("Échec de la connexion : " . mysqli_connect_error()); 
-        } 
-        else
-        { 
-            echo "Connexion réussie à la base de données.";
-        }
-
+            if (!$connexion) { 
+                die("Échec de la connexion : " . mysqli_connect_error()); 
+            } else { 
+                // echo "Connexion réussie à la base de données.";
+            }
+            echo "<table>";
+                    echo "<tr>";
+                    echo "<th>Chanteur</th>";
+                    echo "<th>Album</th>";
+                    echo "<th>Titre</th>";
+                    echo "<th>Année</th>";
+                    echo "<th>Durée</th>";
+                    echo "<th>Lecture</th>";
+                    echo "<th>Télécharger</th>";
+                    echo "</tr>";
             if (isset($_POST['Rechercher'])) {
                 $Rechercher = $_POST['Rechercher'];
+
                 $sql = "SELECT * FROM musique WHERE titre LIKE '%$Rechercher%'";
-                $resultats = mysqli_query($connexion, $sql) ;
-                if($resultats){
+                $resultats = mysqli_query($connexion, $sql);
+                    if($resultats){
+                        foreach ($resultats as $musique) {
+                            echo "<tr>";
+                            echo "<td>" . $musique['chanteur'] . "</td>";
+                            echo "<td>" . $musique['album'] . "</td>";
+                            echo "<td>" . $musique['titre'] . "</td>";
+                            echo "<td>" . $musique['annee'] . "</td>";
+                            echo "<td>" . $musique['duree'] . "</td>";
+                            echo "<td><audio controls src='" . $musique['url'] . "'></audio></td>";
+                            echo "<td><a href='" . $musique['url'] . "'>Télécharger</a></td>";
+                            echo "</tr>";
+                        }
+                    } else{
+                        echo "Aucun résultat trouvé";
+                    }
+            }
+            else {
+                $sql = "SELECT * FROM musique";
+            
+                $resultats = mysqli_query($connexion, $sql);
+                if ($resultats) { 
+                
                     foreach ($resultats as $musique) {
                         echo "<tr>";
                         echo "<td>" . $musique['chanteur'] . "</td>";
@@ -98,56 +122,30 @@
                         echo "<td><audio controls src='" . $musique['url'] . "'></audio></td>";
                         echo "<td><a href='" . $musique['url'] . "'>Télécharger</a></td>";
                         echo "</tr>";
+                       
                     }
-                } else{
-                    echo "Aucun resultat trouvé";
+                  
+                    echo "</table>";
+                } else {
+                    echo "Erreur : " . mysqli_error($connexion); 
                 }
             }
-            else {
-                $sql = "SELECT * FROM musique" ;
-        $resultat = mysqli_query($connexion, $sql) ;
-
-     if ($resultat) { 
-        echo "<table>";
-        echo "<tr>";
-        echo "<th>Chanteur</th>";
-        echo "<th>Album</th>";
-        echo "<th>Titre</th>";
-        echo "<th>Année</th>";
-        echo "<th>Durée</th>";
-        echo "<th>Lecture</th>";
-        echo "<th>Télécharger</th>";
-        echo "</tr>";
-        
-        foreach ($resultat as $musique) {
-            echo "<tr>";
-            echo "<td>" . $musique['chanteur'] . "</td>";
-            echo "<td>" . $musique['album'] . "</td>";
-            echo "<td>" . $musique['titre'] . "</td>";
-            echo "<td>" . $musique['annee'] . "</td>";
-            echo "<td>" . $musique['duree'] . "</td>";
-            echo "<td><audio controls src='" . $musique['url'] . "'></audio></td>";
-            echo "<td><a href='" . $musique['url'] . "'>Télécharger</a></td>";
-            echo "</tr>";
-        }
-        
-        echo "</table>";
-    } else {
-        echo "Erreur : " . mysqli_error($connexion); 
-    }
-            }
-                
-            mysqli_close($connexion);
-                
-    ?>
-    <ul class "lien">
-        <li><a href="contact.html">Contact</a></li>
-        <li><a href="Rechercher.html">Rechercher</a></li>
-        <li><a href="accueil.html">Accueil</a></li>
-    </ul>
+           
+           ?>
     </body>
+
     </html>
 
-    
- 
+
+
+
+
+
+
+
+
+
+
+
+
 
--- Musique/recupere.php
+++ Musique/recupere.php
@@ -1,5 +1,20 @@
 <?php
-$nom = isset($_POST['nom']) ? $_POST['nom'] : '';
+
+$serveur = "localhost";
+$utilisateur = "root"; 
+$mot_de_passe = ""; 
+$base_de_donnees = "base";
+$connexion = mysqli_connect($serveur, $utilisateur, $mot_de_passe, $base_de_donnees);
+if (!$connexion) { 
+    die("Échec de la connexion : " . mysqli_connect_error()); 
+} 
+else
+{ 
+    echo "Connexion réussie à la base de données.";
+}
+
+$nom = isset($_POST['nom']) ? $_POST['nom'] : ''; 
+$prenom = isset($_POST['prenom']) ? $_POST['nom'] : '';
 $email = isset($_POST['email']) ? $_POST['email'] : '';
 $telephone = isset($_POST['telephone']) ? $_POST['telephone'] : '';
 $message = isset($_POST['message']) ? $_POST['message'] : '';
@@ -8,7 +23,10 @@ if (empty($_POST)) {
     echo 'le formulaire n\'a pas été soumis correctement';
 }
 if (!empty($_POST)) {
-    echo "Formulaire soumis avec succès !";
+    echo "Formulaire soumis avec succès !"; 
+    $sql = "INSERT INTO message (prenom, nom, email, telephone, message, objet)
+    VALUES('$prenom','$nom', '$email', '$telephone', '$message', '$objet')" ;
+    mysqli_query($connexion, $sql) ;
 } else {
     echo "Erreur.";
 }
